b985a9dcc65eb13098503535b372b6877c073d53
imranyy <imranmatano6@gmail.com>
2022-07-12 20:46:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+import gradient from 'gradient-string';
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+import { createSpinner } from 'nanospinner';
+
+//console.log(chalk.bgBlue('hello'))
