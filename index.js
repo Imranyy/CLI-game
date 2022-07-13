caae2e5f54857072901c36e39c56b2958fea2937
imranyy <imranmatano6@gmail.com>
2022-07-13 19:46:03 +0300

    game v1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import chalk from 'chalk';
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
@@ -7,10 +6,10 @@ import chalkAnimation from 'chalk-animation';
 import { createSpinner } from 'nanospinner';
 import figlet from 'figlet';
 
-
+let prayerName;
+let number;
 
 //welcome player
-let prayerName;
 const sleep=(ms=2000)=>new Promise((r)=>setTimeout(r,ms));
 async function welcome(){
     const msg='SnoopyPoopyPants\n';
@@ -39,35 +38,165 @@ async function askName(){
     prayerName=answers.player_name;
 };
 
-//question1
+//questions
 async function question1(){
     const answers= await inquirer.prompt({
         name:'question1',
         type:'list',
-        message:'javascript was created in 10days then released on?\n',
+        message:'1:.If Facebook was a country, how would it be in terms of population?\n',
         choices:[
-            'may',
-            'may 23rd 6777'
+            '3 million',
+            '1 billion',
+            '1.4 billion',
+            '700 thousand'
         ]
     });
     
-    return handleAnswer(answers.question1=='may')
+    return handleAnswer(answers.question1=='1 billion')
 };
 
-//question2
 async function question2(){
     const answers=await inquirer.prompt({
         name:'question2',
         type:'list',
-        message:'Who is the Best Programmer?\n',
+        message:'2:.World-wide, what language is used the most on the internet?\n',
         choices:[
-            'imran',
-            'matano',
-            'job'
+            'German',
+            'English',
+            'Spanish',
+            'Chinese'
         ]
     });
-    return handleAnswer(answers.question2=='imran')
+    return handleAnswer(answers.question2=='English')
 }
+
+async function question3(){
+    const answers=await inquirer.prompt({
+        name:'question3',
+        type:'list',
+        message:'3:.True or False: google is the second largest search engine?\n',
+        choices:[
+            'True',
+            'False'
+        ]
+    });
+    return handleAnswer(answers.question3=='False')
+}
+
+async function question4(){
+    const answers=await inquirer.prompt({
+        name:'question4',
+        type:'list',
+        message:'4:.When do chinese students start learning English?\n',
+        choices:[
+            'High school',
+            'Middle school',
+            'Elementary school',
+            'Kindergarten'
+        ]
+    });
+    return handleAnswer(answers.question4=='Kindergarten')
+}
+
+async function question5(){
+    const answers=await inquirer.prompt({
+        name:'question5',
+        type:'list',
+        message:'5:.Twitter reported how many Tweets per day in june 2012?\n',
+        choices:[
+            '2 million',
+            '400 million',
+            '700 thousand',
+            '1 billion'
+        ]
+    });
+    return handleAnswer(answers.question5=='400 million')
+}
+
+async function question6(){
+    const answers=await inquirer.prompt({
+        name:'question6',
+        type:'list',
+        message:'6:.True or False. More people in india speak English than in the United States?\n',
+        choices:[
+            'True',
+            'False'
+        ]
+    });
+    return handleAnswer(answers.question6=='True')
+}
+
+async function question7(){
+    const answers=await inquirer.prompt({
+        name:'question7',
+        type:'list',
+        message:'7:.How many Apps are available on the App store?\n',
+        choices:[
+            '7 billion',
+            '7 million',
+            '700 thousand',
+            '700 million'
+        ]
+    });
+    return handleAnswer(answers.question7=='700 thousand')
+}
+
+async function question8(){
+    const answers=await inquirer.prompt({
+        name:'question8',
+        type:'list',
+        message:'8:.About how many smartphones are being used in the world?\n',
+        choices:[
+            '1 million',
+            '1 billion',
+            '100 billion',
+            '100 billion'
+        ]
+    });
+    return handleAnswer(answers.question8=='1 billion')
+}
+
+async function question9(){
+    const answers=await inquirer.prompt({
+        name:'question9',
+        type:'list',
+        message:'9:.If wikipedia was a book how many pages would it be?\n',
+        choices:[
+            '1 billion',
+            '400 million',
+            '2 million'
+        ]
+    });
+    return handleAnswer(answers.question9=='2 million')
+}
+
+async function question10(){
+    const answers=await inquirer.prompt({
+        name:'question10',
+        type:'list',
+        message:'10:.What runs and does not have legs?\n',
+        choices:[
+            'Acids',
+            'Liquids',
+            'Water',
+            'Lava'
+        ]
+    });
+    return handleAnswer(answers.question10=='Water')
+}
+
+/*async function last(){
+    const answer=await inquirer.prompt({
+        name:'last',
+        type:'input',
+        message:'What is Your WhatsApp Number',
+        default(){
+            return 'I didnt wanna👿👿!'
+        }
+    })
+    number=answer.last;
+}*/
+
 
 //handle answer
 async function handleAnswer(isCorrect){
@@ -99,6 +228,7 @@ function winner(){
     figlet(msg,(err,data)=>{
         console.log(gradient.pastel.multiline(data));
     });
+    console.log(chalk.yellow('https://wa.me/+254754423664'))
 }
 
 //calling functions
@@ -106,4 +236,13 @@ await welcome();
 await askName();
 await question1();
 await question2();
+await question3();
+await question4();
+await question5();
+await question6();
+await question7();
+await question8();
+await question9();
+await question10();
+//await last();
 await winner();
